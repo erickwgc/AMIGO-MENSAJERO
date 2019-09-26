4ba5e7dcbd6b89651c87383a924040c6827fe584
wildercanaviri <wildersistemas20@gmail.com>
2019-09-26 12:34:44 -0400

    adaptacion a laraval 5.2

---
+++ blog/public/js/galeria_arrastre.js
@@ -0,0 +1,44 @@
+// JavaScript Document
+var elem_destino;
+function comenzar(){
+	
+	var imagenes=document.querySelectorAll("#cajaimagen img");
+	for(var i=0;i<imagenes.length;i++){
+		imagenes[i].addEventListener("dragstart",comenzando_arrastrar,false);
+		
+		
+	}
+	elem_destino=document.getElementById("zonadestino");
+	elem_destino.addEventListener("dragenter",entrando,false);
+	
+	elem_destino.addEventListener("dragover",function(e){
+		e.preventDefault();},false);
+	elem_destino.addEventListener("drop",soltado,false);
+	elem_destino.addEventListener("dragleave",saliendo,false);
+}
+function comenzando_arrastrar(e){
+	var elemento=e.target;
+	e.dataTransfer.setData("Text",elemento.getAttribute("id"));
+	
+}
+
+function soltado(e){
+	e.preventDefault();
+	var id=e.dataTransfer.getData("Text");
+	var src=document.getElementById(id).src;
+	elem_destino.innerHTML="<img src='"+src+"' width: '150px' height: '150px'>";
+	elem_destino.style.background="#ffffff";
+
+}
+
+function entrando(e){ 
+	e.preventDefault();
+	elem_destino.style.background="#C2FF33";
+}
+
+function saliendo(e){
+	e.preventDefault();
+	elem_destino.style.background="#ffffff";
+}
+
+window.addEventListener("load",comenzar,false);
